ab53f666dfb0fa9ab6ae93d8efdf3c6ce6339bc8
mpq <m3atyy@gmail.com>
2022-12-03 15:33:30 +0500

    add colors to ios app

---
+++ iosApp/iosApp/Extensions/Colors.swift
@@ -0,0 +1,14 @@
+//
+//  Colors.swift
+//  iosApp
+//
+//  Created by Slava Rykov on 03.12.2022.
+//  Copyright © 2022 orgName. All rights reserved.
+//
+
+import SwiftUI
+
+extension Color {
+    static let AppRed = Color("AppRed")
+    static let AppWhite = Color("AppWhite")
+}
