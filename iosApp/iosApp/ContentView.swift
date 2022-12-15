d839f0e97a2199858a51bf2c8eada239b5ac8945
ukenov <ukenov@flagstudio.ru>
2022-12-16 00:42:55 +0500

    remove navbar to iosApp (ya yebal yetot swiftUI)

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -6,13 +6,17 @@ struct ContentView: View {
     @StateObject var pilot = UIPilot(initial: AppRoute.Welcome)
     
 	var body: some View {
-        UIPilotHost(pilot) { route in
-            switch route {
-            case .Welcome: WelcomeScreen()
-            case .Auth: AuthScreen()
-            case .AuthInterests: InterestsScreen()
-            case .Profile: UserProfileScreen()
+            UIPilotHost(pilot) { route in
+                switch route {
+                case .Welcome: WelcomeScreen().navigationBarBackButtonHidden(true).uipNavigationBarHidden(true)
+                case .Auth: AuthScreen().navigationBarBackButtonHidden(true).uipNavigationBarHidden(true)
+                case .AuthInterests: InterestsScreen().navigationBarBackButtonHidden(true).uipNavigationBarHidden(true)
+                case .Profile: UserProfileScreen().navigationBarBackButtonHidden(true).uipNavigationBarHidden(true)
+                }
             }
-        }
+            .ignoresSafeArea()
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .padding(.top, -16)
+       
 	}
 }
